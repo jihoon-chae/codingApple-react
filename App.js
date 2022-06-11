0feab890a9494b31085336bf909f11f40e0985bb
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-06-11 13:54:05 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0)
+  let [입력값, 입력값변경] = useState('') // 인풋값에 쓸 것들
 
   return (
     <div className="App">
@@ -40,7 +41,7 @@ function App() {
             >
               {글제목[i]}
               <span
-                onClick={() => {
+                onClick={(e) => {e.stopPropagation() // 좋아요 누르면 모달창 나오는거 막으려고
                   따봉변경(따봉 + 1);
                 }}
               >
@@ -49,10 +50,22 @@ function App() {
               {따봉}{" "}
             </h4>
             <p>2월 17일 발행</p>
+            <button onClick={() => { // 글삭제 버튼
+              let copy = [...글제목]
+              copy.splice(i, 1) // i라는 변수를 써야 i번째 삭제버튼 누르면 i번째 글이 삭제됨
+              글제목변경(copy)
+            }}>삭제</button>
           </div>
         );
       })}
 
+<input onChange={(e) => {입력값변경(e.target.value)}} />
+
+<button onClick={()=> {
+  let copy = [...글제목];
+  copy.unshift(입력값); // e.target.value 가 unshift됨
+  글제목변경(copy)}}>글발행</button> 
+{/* 글제목에 하나만 더해주면 글 개수가 늘어남 */}
       {modal == true ? <Modal title={title} 글제목={글제목} /> : null}
     </div>
   );
