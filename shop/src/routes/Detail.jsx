73082babe038cb79cfd0de326c32b51fd790b99e
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-06-13 09:30:20 +0900

    useEffect

--- shop/src/routes/Detail.jsx
+++ shop/src/routes/Detail.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
-  let {id} =  useParams(); // 유저가 URL파라미터 입력한거 가져오기
-console.log(id)
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let { id } = useParams(); // 유저가 URL파라미터 입력한거 가져오기
+
+  useEffect(() => { // mount, update시 실행됨 
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    console.log(2)
+    return () => {
+      console.log(1) // useEffect 동적전에 실행되는 코드
+      clearTimeout(a); // 타이머 제거해주는 함수, mount시 실행 안됨
+    }; 
+  }, []);
+
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className="row">
         <div className="col-md-6">
           <img
