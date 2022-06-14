5ee61bcadc0f02485845774b0e4d3fc7ed6d1eb8
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-06-15 00:54:05 +0900

    성능개선 1 : 개발자도구 & lazy import

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState, useEffect } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-import Detail from "./routes/Detail";
 import axios from "axios";
-import Cart from './routes/Cart';
+import Detail from "./routes/Detail";
+import Cart from "./routes/Cart";
+// lazy문법으로 inport해서 첫 페이지 로딩속도 향상
+const Detail = lazy(() => import("./routes/Detail.js"));
+const Cart = lazy(() => import("./routes/Cart.js"));
 
 function App() {
-
-  useEffect(()=>{
-    localStorage.setItem('watched', JSON.stringify( [] ))
-  },[])
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify([]));
+  }, []);
 
   let [shoes, setShoes] = useState(data); // data.js에 있는 어레이
   let navigate = useNavigate(); // 페이지 이동시킬때 Link대신 씀, Hook
@@ -63,21 +65,19 @@ function App() {
                     .get("https://codingapple1.github.io/shop/data2.json")
                     .then((결과) => {
                       // console.log(결과.data);
-                    
+
                       let copy = [...shoes, ...결과.data]; // axios가 JSON을 array로 자동으로 바꿔줌
                       // console.log(copy)
-                      setShoes(copy)
+                      setShoes(copy);
                       // 로딩중 UI 숨기기
-                    })
+                    });
 
-                    axios.post('url', {name : 'kim'}) // 서버로 데이터 전송
+                  axios.post("url", { name: "kim" }); // 서버로 데이터 전송
 
-                    Promise.all([axios.get('/url1'), axios.get('/url2')]) // 동시에 ajax요청
-                    .then(() => {
+                  Promise.all([axios.get("/url1"), axios.get("/url2")]) // 동시에 ajax요청
+                    .then(() => {})
 
-                    })
-
-                    .catch(() => {  
+                    .catch(() => {
                       console.log("실패함");
                     });
                 }}
@@ -87,7 +87,10 @@ function App() {
             </div>
           }
         />{" "}
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Suspense> 
+          {/* Suspense로 감싸면 해당 컴포넌트 접속시 로딩중 UI넣기 가능 */}
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        </Suspense>
         {/* /detail로 접속하면 보여줄 html */}
         <Route path="*" element={<div>404, 없는 페이지임</div>} />
         {/* 404페이지 */}
@@ -97,7 +100,7 @@ function App() {
           <Route path="location" element={<About />} />
           {/* about인데 location에 대한 설명, 라우트태그 안에 넣기 */}
         </Route>
-        <Route path='/cart' element={<Cart />}/>
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
